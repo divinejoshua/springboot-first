5b221230e555965159cf2a1b257d9670c0505fc2
divinejoshua <divineuche399@gmail.com>
2023-08-22 11:25:23 +0100

    Added home page

--- src/main/java/com/springbootfirst/controllers/WelcomeController.java
+++ src/main/java/com/springbootfirst/controllers/WelcomeController.java
@@ -1,5 +1,16 @@
 package com.springbootfirst.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+	
+	@GetMapping("/")
+	public String welcome() {
+		
+		return "Welcome to Divine's new project. This is Spring boot Java.";
+		
+	}
 
 }
