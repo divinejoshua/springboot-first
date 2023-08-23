38613acf0c20d2a6300489735fbf2cabc88ce6fb
divinejoshua <divineuche399@gmail.com>
2023-08-23 11:08:55 +0100

    Added User controller

--- src/main/java/com/springbootfirst/controllers/UserController.java
+++ src/main/java/com/springbootfirst/controllers/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class UserController {
 	
+	
+//	Home page 
 	@GetMapping("/")
 	public String welcome() {
 		
